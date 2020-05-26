2136fc519ea89d8bccd5405a95b5e4cd42e237a9
Stefan Grozev <stefan.grozev@resolve.io>
2020-05-26 16:17:04 +0300

    Add project files.

---
+++ Project5/Header.h
@@ -0,0 +1,9 @@
+#ifdef LIBRARY_EXPORTS
+#    define LIBRARY_API __declspec(dllexport)
+#else
+#    define LIBRARY_API __declspec(dllimport)
+#endif
+
+LIBRARY_API void calculateHighestExpense();
+LIBRARY_API void calculateMonthlyExpense();
+
